86230dacc5fa0d2cf679e305f74faacb37e6380e
sarker24 <sarker24@gmail.com>
2023-07-16 18:49:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import  AppFooter  from './components/AppFooter.vue'
+import AppHome from './components/AppHome.vue'
+
+
+</script>
+
+<template>
+ <AppHeader />
+ <main class="container">
+   <AppHome />
+  </main>
+ <AppFooter />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+<template>
+   <div class="container mx-auto mx-32  ">
+    <div class="grid grid-cols-2 gap-16 my-12">
+      <div class="">
+       <h1 class="text-6xl">Promote Your Products <br> by Colorlib</h1>
+       <p class="my-12 font-mono text-slate-400">
+        Product text includes any content that is used to describe a product.
+         Product descriptions are the most common example, but there are other
+          forms of this kind of text that include advertorials, blog posts, 
+          and reviews. The ultimate goal of such texts is to sell more of
+          the product being featured in it.
+       </p>
+       
+        <button class="rounded-none bg-sky-500 text-white p-2  mr-3">See more</button>
+       <button class="rounded-none border-solid border-2 border-sky-600 text-sky-500 p-2">Pricing</button>
+       
+       
+      </div> 
+      <div class="">
+        <img src="https://img.freepik.com/free-vector/male-animator-sitting-computer-desk-creating-project-graphic-motion-designer-sitting-workplace-studio-developing-web-game-flat-vector-illustration-design-art-concept_74855-22510.jpg?w=1380&t=st=1689526014~exp=1689526614~hmac=b5453c62db7af07c9883cec38b678cf0621ccd13c5c35445ac4494ddda294199" alt="">
+    </div>
+    </div>
+   </div>
+</template> 
+<style scoped></style>
