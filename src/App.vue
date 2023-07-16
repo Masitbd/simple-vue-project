b02299c438983eb68c816c13cc431ce33a10da7a
sarker24 <sarker24@gmail.com>
2023-07-16 19:41:46 +0200

    Add pricing plan

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import AppHeader from './components/AppHeader.vue';
 import  AppFooter  from './components/AppFooter.vue'
 import AppHome from './components/AppHome.vue'
 import AppProject from './components/AppProject.vue'
+import AppPricing from './components/AppPricing.vue'
 
 
 </script>
@@ -12,6 +13,7 @@ import AppProject from './components/AppProject.vue'
  <main class="container mx-auto mx-32 ">
    <AppHome />
    <AppProject />
+   <AppPricing/>
   </main>
  <AppFooter />
 </template>
